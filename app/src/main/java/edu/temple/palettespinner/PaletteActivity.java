a747fc3aeea7310fe1a4339262fe2e0409b27ba1
William Power <willpowe@gmail.com>
2017-02-09 10:26:23 -0500

    Initial commit

---
+++ app/src/main/java/edu/temple/palettespinner/PaletteActivity.java
@@ -0,0 +1,50 @@
+package edu.temple.palettespinner;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+
+public class PaletteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_palette);
+
+        final ArrayList<String> colors = new ArrayList<>();
+        colors.add("White");
+        colors.add("Blue");
+        colors.add("Green");
+        colors.add("Magenta");
+        colors.add("Purple");
+
+        final View layout = findViewById(R.id.activity_palette);
+        Spinner spin = (Spinner) findViewById(R.id.color_spinner);
+        PaletteAdapter pa = new PaletteAdapter( this, colors );
+        spin.setAdapter( pa );
+
+        spin.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                try {
+                    layout.setBackgroundColor(Color.parseColor(colors.get(position)));
+                } catch (Exception e ){
+
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                layout.setBackgroundColor(Color.WHITE);
+            }
+        });
+
+    }
+
+
+}
