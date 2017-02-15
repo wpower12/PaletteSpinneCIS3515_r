e9bea824d13c171688b5767ca1262bfec47f9ee3
William Power <willpowe@gmail.com>
2017-02-15 13:39:51 -0500

    Fixed initial color selection.

--- app/src/main/java/edu/temple/palettespinner/PaletteActivity.java
+++ app/src/main/java/edu/temple/palettespinner/PaletteActivity.java
@@ -11,12 +11,15 @@ import java.util.ArrayList;
 
 public class PaletteActivity extends AppCompatActivity {
 
+    private boolean first_fire = true;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_palette);
 
         final ArrayList<String> colors = new ArrayList<>();
+        colors.add("Red");
         colors.add("White");
         colors.add("Blue");
         colors.add("Green");
@@ -31,6 +34,12 @@ public class PaletteActivity extends AppCompatActivity {
         spin.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                if( first_fire ) {
+                    first_fire = false;
+                    return;
+                }
+
                 try {
                     layout.setBackgroundColor(Color.parseColor(colors.get(position)));
                 } catch (Exception e ){
